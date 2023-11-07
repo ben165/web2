19629a59c3961229d9839c4da70f673edbf240c7
ben165 <urich098@gmail.com>
2023-11-07 22:09:01 +0100

    night session

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const sqlite3 = require('sqlite3')
 const express = require('express')
 const app = express();
 
+//import {State} from 'helper.js'
+
+//console.log(__dirname)
+
 // OPTIONS
 app.set('view engine', 'ejs') // register view engine
 
@@ -29,7 +33,7 @@ app.get("/", (req, res) => {
 })
 
 
-app.post("/create", (req, res) => {
+app.get("/create", (req, res) => {
     //console.log(req.body)
 
     // Hier fehlen Abfragen:
@@ -37,12 +41,21 @@ app.post("/create", (req, res) => {
     // - Check if gameid is greater than 3
     // - Check if gameid is already taken
 
-    emptySet = "0000000000000000000000000000000000000000000000000000000000000000"
+    emptySet = "0000000000000000000000000001200000021000000000000000000000000000"
+
+    let state = new State(0,1,emptySet);
+    let moveSet = state.moveStr()
+
+    console.log(moveSet)
+
+    let counter = state.turn
+    let player = state.player
+
     gameid = req.body.gameid
 
-    sql = `insert into game(gameid, state, current, winner) values (?,?,?,?)`
+    sql = `insert into game(gameid, state, current, winner, moveStr, counter) values (?,?,?,?,?,?)`
     
-    db.run(sql,[gameid, emptySet, 0, 0],(err) => {
+    db.run(sql,["game02", emptySet, player, 0, moveSet, counter],(err) => {
         if (err) return console.log(err.message)
     })
 
@@ -56,7 +69,7 @@ app.get("/cancel", (req, res) => {
     emptySet = "0000000000000000000000000000000000000000000000000000000000000000"
     //gameid = req.body.gameid // not needed cause there is only one game...
 
-    sql = `UPDATE game SET state = ?, current = ?, winner = ? WHERE gameid = "123"`
+    sql = `UPDATE game SET state = ?, current = ?, winner = ? WHERE gameid = "game01"`
 
     db.run(sql,[emptySet, 0, 0],(err) => {
         if (err) return console.log(err.message)
@@ -114,30 +127,50 @@ app.get("/gameinfo/:gameid", (req, res) => {
 
 
 app.post("/makeMove", (req, res) => {
-    move = req.body
 
-    console.log(move)
+    /*
+    {
+        "player": 1,
+        "move": 30
+    }
+    
+    Von Datenbank:
+    - state (boardStr)
+    - moveStr (was erlaubt ist)
+    - currentPlayer, db: current
+    - game02
+    */
 
-    pos = req.body.pos
-    player = req.body.player
-    console.log(typeof(player))
-    gameid = req.body.gameid
+    playerMove = req.body.move
+    playerId = req.body.player
 
-    sql = `select state from game where gameid = ?`
+    sql = `select state, moveStr, current, counter from game where gameid like "game02"`
 
-    db.all(sql,[gameid],(err, rows) => {
+    db.all(sql,[],(err, rows) => {
         if (err) return console.log(err.message)
         rows.forEach(row=>{
-            state = row.state
-            if (player == 1) {
-                state = state.substring(0, pos) + "1" + state.substring(pos+1, state.length)
-            } else {
-                state =  state.substring(0, pos) + "2" + state.substring(pos+1, state.length)
+            let stateDb = row.state
+            let playerDb = row.current
+            let moveStrDb = row.moveStr
+            let counterDb = row.counter
+
+            console.log(playerDb, playerId, moveStrDb[playerMove], 1)
+            if (playerDb === playerId && moveStrDb[playerMove] === "1") {
+                console.log("HEEERE")
+                let state = new State(counterDb, playerDb, stateDb)
+                state.makeMove(playerMove)
+                let newMoveStr = state.moveStr()
+                let newState = state.boardStr
+                let newPlayer = state.player
+                let newCounter = counterDb + 1
+                
+                console.log(newState)
+
+                sql = `update game set moveStr = ?, state = ?, current = ?, counter = ? where gameid like "game02"`
+                db.run(sql, [newMoveStr, newState, newPlayer, newCounter], (err) => {
+                    if (err) return console.log(err.message)
+                })
             }
-            sql = `update game set state = ? where gameid = ?`
-            db.run(sql, [state, gameid], (err) => {
-                if (err) return console.log(err.message)
-            })
         })
     })
     res.json({ "ok": 1 })
@@ -167,3 +200,126 @@ app.use( (req, res) =>{
 
 app.listen(3000)
 
+
+
+
+
+
+
+
+
+
+
+
+const n = 8;
+
+ class BoardElement {
+    constructor(x, y, stone) {
+        this.x = x;
+        this.y = y;
+        this.stone = stone;  // 0 leer, 1 Spieler1, 2 Spieler2
+        this.flips = []; // integer
+    }
+
+    setPossibleFlips(flips) {
+        this.flips = flips;
+    }
+
+    getIndex() {
+        return n * this.y + this.x;
+    }
+}
+
+ class State {
+    constructor(turn, player, boardStr) {
+        this.boardStr = boardStr;
+        this.player = player; // 1 oder 2
+        this.turn = turn;
+        this.board = [];
+        for (let i = 0; i < n * n; i++) {
+            this.board.push(new BoardElement(i % 8, (i - i % 8) / 8, Number(this.boardStr[i])))
+        }
+        this.setPossibleFlips();
+    }
+
+    moveStr() {
+        let moveStr = "";
+        let c;
+        for(let i = 0; i < this.board.length; i++) {
+            c = this.board[i].flips.length === 0 ? 0 : 1;
+            moveStr += c;
+        }
+        return moveStr;
+    }
+
+    makeMove(k) {
+        let flips = this.board[k].flips;
+        if (flips.length > 0) {
+            let newBoardStr = Array.from(this.boardStr);
+            for (let i = 0; i < flips.length; i++) {
+                newBoardStr[flips[i]] = this.player;
+                this.board[flips[i]].stone = this.player;
+                this.board[flips[i]].flips = [];
+            }
+            this.boardStr = newBoardStr.join("");
+            this.player = this.otherPlayer();
+            this.turn += 1;
+            this.setPossibleFlips();
+        }   
+    }
+
+    setPossibleFlips() {
+        for (const i in this.board) {
+            this.board[i].setPossibleFlips(this.possibleFlips(Number(i)));
+        }
+    }
+
+    possibleFlips(i) {
+        let elm = this.board[i];
+        let result = [];
+        if (elm.stone === 0) {
+            const dd = [[1, 0], [1, 1], [0, 1], [-1, 1,], [-1, 0], [-1, -1], [0, -1], [1, -1]];
+            for (const d in dd) {
+                let temp = [];
+                let nextI = i;
+                let nextStone;
+                while (true) {
+                    nextI = this.neighbor(nextI, dd[d]);
+                    if (nextI === -1) {
+                        temp = [];
+                        break;
+                    }
+                    nextStone = this.board[nextI].stone;
+                    if (nextStone === this.player) {
+                        break;
+                    }
+                    if (nextStone === 0) {
+                        temp = [];
+                        break;
+                    }
+                    temp.push(nextI);
+                }
+                result = result.concat(temp);
+            }
+        }
+        if (result.length !== 0) {
+            result.push(i);
+        }
+        return result;
+    }
+
+    neighbor(i, d) {
+        let elm = this.board[i]
+        const newX = elm.x + d[0];
+        const newY = elm.y + d[1];
+        if (newX < 0 || newX >= n || newY < 0 || newY >= n) {
+            return -1;
+        } else {
+            return (n * newY + newX);
+        }
+    }
+
+    otherPlayer() {
+        return this.player === 1 ? 2 : 1;
+    }
+}
